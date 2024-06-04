5e9ded564fcbf3d86d175dc90a099800358daa55
niclashart <niclas.hart@online.de>
2024-06-04 11:28:46 +0200

    commit

--- DeepL_Potato_Disease/api/main.py
+++ DeepL_Potato_Disease/api/main.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("")
+MODEL = tf.keras.models.load_model("../saved_models/1")
+CLASS_NAMES = ['Early Blight', 'Late Blight', 'Healthy']
 
 @app.get('/ping')
 async def ping():
@@ -20,7 +21,12 @@ def read_files_as_image(data) -> np.ndarray:
 
 @app.post('/ping')
 async def predict(file: UploadFile = File(...)):
-    bytes = file.read()
+    image = read_files_as_image(await file.read())
+    
+    img_batch = np.expand_dims(image, 0)
+    
+    prediction = MODEL.predict(img_batch)
+    pass
 
 if __name__ =="__main__":
     uvicorn.run(app, host='localhost', port=8000)
