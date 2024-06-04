a69df12820e47b261dbaed5c5b4ffde9c88dcb64
niclashart <niclas.hart@online.de>
2024-06-04 11:10:52 +0200

    commit

--- DeepL_Potato_Disease/api/main.py
+++ DeepL_Potato_Disease/api/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
+
+MODEL = tf.keras.models.load_model("")
 
 @app.get('/ping')
 async def ping():
     return 'Hello, I am alive'
+
+def read_files_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
 
 @app.post('/ping')
 async def predict(file: UploadFile = File(...)):
